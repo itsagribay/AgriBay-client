1e77c930cc0a8b6fe377d088b7f7ac3aaf516795
Anurag Srivastava <anuragsrivastava0009@gmail.com>
2021-01-16 12:59:00 +0530

    authentication front-end almost done

--- agribay-app/src/app/app.module.ts
+++ agribay-app/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { ProductService } from "./services/product.service";
 
 import { Routes, RouterModule } from "@angular/router";
@@ -15,8 +15,18 @@ import { ProductDetailsComponent } from './components/product-details/product-de
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CartStatusComponent } from './components/cart-status/cart-status.component';
 import { CartDetailsComponent } from './components/cart-details/cart-details.component';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgxWebstorageModule } from 'ngx-webstorage';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { TokenInterceptor } from './auth/token-interceptor';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { UserProfileComponent } from './auth/user-profile/user-profile.component';
+//import { EditorModule } from '@tinymce/tinymce-angular';
+
 
 const routes: Routes = [
   {path: 'cart-details', component: CartDetailsComponent },
@@ -25,9 +35,9 @@ const routes: Routes = [
   {path: 'category/:itemCategory', component: ProductListComponent},
   {path: 'category', component: ProductListComponent},
   {path: 'products', component: ProductListComponent},
-  {path: 'login', component: LoginComponent },
-  {path: 'signup', component: SignupComponent },
-  {path: '', redirectTo: '/products', pathMatch: 'full'},
+  {path: 'signup', component: SignupComponent},
+  {path: 'login', component: LoginComponent},
+   {path: '', redirectTo: '/products', pathMatch: 'full'},
   {path: '**', redirectTo: '/products', pathMatch: 'full'},
   
 ];
@@ -41,16 +51,36 @@ const routes: Routes = [
     ProductDetailsComponent,
     CartStatusComponent,
     CartDetailsComponent,
+    SignupComponent,
     LoginComponent,
-    SignupComponent
+    UserProfileComponent
+    
   ],
   imports: [
     RouterModule.forRoot(routes),
     BrowserModule,
     HttpClientModule,
-    NgbModule
+    NgxWebstorageModule.forRoot(),
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    FontAwesomeModule,
+    //EditorModule,
+    NgbModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    
+
   ],
-  providers: [ProductService],
+
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true
+    }
+  ,ProductService],
   bootstrap: [AppComponent]
+ // providers: [ProductService],
+  
 })
 export class AppModule { }
